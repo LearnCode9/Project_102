b2c1110f93ffcf4387072159dab24c83d69380a4
LearnCode9 <86936555+LearnCode9@users.noreply.github.com>
2022-08-18 18:15:50 -0400

    Add files via upload

---
+++ LetsChat_page.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDhCMCP4XVtyZYNMfs3olzqhrFEVya8vtw",
+    authDomain: "project-101-45ad9.firebaseapp.com",
+    databaseURL: "https://project-101-45ad9-default-rtdb.firebaseio.com",
+    projectId: "project-101-45ad9",
+    storageBucket: "project-101-45ad9.appspot.com",
+    messagingSenderId: "695772828776",
+    appId: "1:695772828776:web:2140a47018d14c947797dd"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+	user_name = localStorage.getItem("user_name");
+	room_name = localStorage.getItem("room_name");
+
+  
